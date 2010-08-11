b3e01aa39c07d986fec481e2d6c834303e597cf4
Aram Dulyan <aram00@gmail.com>
2010-08-11 16:58:58 +1000

    Model cleanup and files now titled.

--- models.py
+++ models.py
@@ -8,8 +8,9 @@ from easy_thumbnails.files import get_thumbnailer
 
 from base import *
 from forms import TextileContentAdminForm, ImageForm
+from django.template.loader import render_to_string
 
-#__all__ = ['TextContent', 'DownloadableContent', 'ImageContent', 'AudioContent', 'VideoContent']
+__all__ = ['LumpyContent', 'HierarchicalLumpyContent', 'Reusable', 'OneOff', 'TextContent', 'DownloadableContent', 'ImageContent', 'AudioContent', 'VideoContent']
 
 class Reusable(object):
 	__metaclass__ = ReusableBase
@@ -26,11 +27,12 @@ class OneOff(object):
 MAX_ALT_TEXT_LENGTH = 1024
 
 UPLOAD_PATH = getattr(settings, 'UPLOAD_PATH', 'uploads/')
+TEXT_FORMATTER = getattr(settings, 'TEXT_FORMATTER', lambda self, content: content)
 
 class TextContent(models.Model):
 	content = models.TextField()
 
-	formatter = lambda x: x
+	formatter = TEXT_FORMATTER
 	content_field_name = 'text_block'
 	
 	class Meta:
@@ -38,8 +40,6 @@ class TextContent(models.Model):
 		verbose_name = _("Text Block")
 
 	def render(self, **kwargs):
-		# this should possibly be done via a call to smartembed/textile
-		# methods directly; just directly replacing the templatetag for now
 		return self.formatter(self.content)
 
 	form = TextileContentAdminForm
@@ -48,42 +48,42 @@ class TextContent(models.Model):
 	feincms_item_editor_includes = {
 		'head': [ 'feincmstools/textilecontent/init.html' ],
 		}
-	
 
-class DownloadableContent(models.Model):
-	link_text = models.CharField(max_length=255)
-	downloadable = models.FileField(upload_to=UPLOAD_PATH+'file/%Y/%m/%d/')
-	include_icon = models.BooleanField(default=True)
+class AbstractFile(models.Model):
+	title = models.CharField(max_length=255, blank=True, help_text=_('The filename will be used if not given.'))
+	
+	with_extension = False
+	
+	class Meta:
+		abstract = True
+	
+	def __unicode__(self):
+		return self.get_title()
+	
+	def get_title(self):
+		if self.title:
+			return self.title
+		if hasattr(self, file):
+			return self.file.name if self.with_extension else os.path.splitext(self.file.name)[1]
+		return None
+	
+class DownloadableContent(AbstractFile):
+	file = models.FileField(upload_to=UPLOAD_PATH+'file/%Y/%m/%d/')
 	
 	content_field_name = 'file'
+	with_extension = True
 	
-	def get_file_name(self):
-		return (os.path.split(self.downloadable.file.name)[1])
-
-	def get_file_extension(self):
-		extension = os.path.splitext(self.downloadable.file.name)[1][1:].lower()
-		if extension in ['ppt','pptx','pptm','pot','potx','potm','pps','ppsx','ppsm','key']:
-			extension = 'ppt'
-		elif extension in ['pdf']:
-			extension = 'pdf'
-		else:
-			extension = 'generic'
-		return extension
-
 	class Meta:
 		abstract = True
 		verbose_name = "Downloadable File"
 		verbose_name_plural = "Downloadable Files"
 
-	# def render(self, **kwargs):
-	#	 downloadable = self.downloadable
-	#	 template = get_template("lumpypages/downloadable.html")
-	#	 c = Context({'downloadable': {'file': self.downloadable, 'link_text': self.link_text, 'include_icon': self.include_icon, 'filename': self.get_file_name(), 'file_extension': self.get_file_extension()}})
-	#	 return template.render(c)
+	def render(self, **kwargs):
+		return render_to_string('feincmstools/content/file.html', dict(file=self))
 
 # --- Media models ------------------------------------------------------------
 
-class ImageContent(models.Model):
+class ImageContent(AbstractFile):
 	file = models.ImageField(upload_to=UPLOAD_PATH+'images/%Y/%m/%d/',
 							 height_field='file_height', width_field='file_width',
 							 max_length=255)
@@ -100,12 +100,15 @@ class ImageContent(models.Model):
 	class Meta:
 		abstract = True
 	
+	def render(self, **kwargs):
+		return render_to_string('feincmstools/content/image.html', dict(image=self))
+	
 	def get_thumbnail(self, **kwargs):
 		options = dict(size=(100, 100), crop=True)
 		options.update(kwargs)
 		return get_thumbnailer(self.file).get_thumbnail(options)
 
-class VideoContent(models.Model):
+class VideoContent(AbstractFile):
 	file = models.FileField(upload_to=UPLOAD_PATH+'video/%Y/%m/%d/', max_length=255)
 	image = models.ImageField(upload_to=UPLOAD_PATH+'video/%Y/%m/%d/still_image/', max_length=255, blank=True)
 	
