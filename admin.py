be854d5d906fa3d358c3ef7b7a020d11fd84155b
Aram Dulyan <aram00@gmail.com>
2010-08-17 12:12:21 +1000

    Copied the last of unreplicated functionality from the deprecated media app and deleted it.

--- admin.py
+++ admin.py
@@ -20,3 +20,15 @@ class HierarchicalLumpyContentAdmin(LumpyContentAdmin, editor.TreeEditor):
             actions.insert(0, u'<a href="%s" title="%s"><img src="%simg/admin/selector-search.gif" alt="%s" /></a>' % (content.get_absolute_url(), _('View on site'), settings.ADMIN_MEDIA_PREFIX, _('View on site')))
         return actions
 
+ADMIN_THUMBNAIL_SIZE = (100, 100)
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'admin_thumbnail',)
+    list_filter = ('category',)
+    search_fields = ('name', 'category__name',)
+    
+    def admin_thumbnail(self, image):
+        return '<img src="%s" />' % (
+            image.get_thumbnail(size=ADMIN_THUMBNAIL_SIZE).url)
+    admin_thumbnail.allow_tags = True
+    admin_thumbnail.short_description = 'Thumbnail'
