61d6ed767d3c3e413f793ff649dcf875f8dec270
Julien Phalip <jphalip@gmail.com>
2010-08-19 16:23:31 +1000

    Added more controls for images (resizing, floating, etc.).

--- mixins.py
+++ mixins.py
@@ -3,53 +3,43 @@ from django.db import models
 from django.template.loader import render_to_string
 
 class FriendlyNamed(models.Model):
-	name = models.CharField('Friendly name', max_length=255, help_text='Used in the admin interface only')
-	
-	class Meta:
-		abstract = True
-	
-	def __unicode__(self):
-		return self.name
+    name = models.CharField('Friendly name', max_length=255, help_text='Used in the admin interface only')
+    
+    class Meta:
+        abstract = True
+    
+    def __unicode__(self):
+        return self.name
 
 
 MAX_CAPTION_LENGTH = 1024
 
 class ImageUseMixIn(models.Model):
-	IMAGE_POSITIONS = (
-		('L', 'Left'),
-		('R', 'Right'),
-		('C', 'Centre'),
-		('B', 'Block'),
-		('T', 'Thumbnail'),
-		)
-	
-	caption = models.CharField(max_length=MAX_CAPTION_LENGTH, blank=True)
-	link_to_original = models.BooleanField(
-		default=False, help_text='Allow users to download original file?')
-	position = models.CharField(max_length=1, choices=IMAGE_POSITIONS, default='B')
-	
-	render_template = 'feincmstools/content/imagemixin.html'
-	
-	class Meta:
-		abstract = True
-	
-	def rendersize(self):
-		"""
-		Returns a tuple (w, h) to which to resize the image
-		"""
-		return {
-			'L': (212, 10000), #TODO: cheat!!
-			'R': (222, 10000),
-			'C': (212, 10000),
-			'B': (444, 10000),
-			'T': (202, 202),
-		}[self.position]
-	
-	def css_classes(self):
-		""" Return space-separated list of css classes for this image. """
-		css = []
-		position = dict(self.IMAGE_POSITIONS).get(self.position, '')
-		css.append(position.lower())
-		return ' '.join(css)
-
-
+    IMAGE_POSITIONS = (
+        ('L', 'Left'),
+        ('R', 'Right'),
+        ('C', 'Centre'),
+        )
+    EXPAND_OPTIONS = (
+        ('100%', '100%'),
+        ('75%', '75%'),
+        ('50%', '50%'),
+        ('33%', '33%'),
+        ('25%', '25%'),
+    )
+    
+    caption = models.CharField(max_length=MAX_CAPTION_LENGTH, blank=True)
+    link_to_original = models.BooleanField(
+        default=False, help_text='Allow users to download original file?')
+    position = models.CharField(max_length=1, choices=IMAGE_POSITIONS, blank=True)
+    wrappable = models.BooleanField(default=False, blank=True, help_text="Tick to let the following item wrap around the image")
+    
+    # Move these to the NFSA app or to a different mixin?
+    expand = models.CharField(max_length=50, choices=EXPAND_OPTIONS, blank=True, help_text="Expands the image's width relatively to its container")
+    width = models.PositiveIntegerField(blank=True, null=True, help_text="Forces the width to a certain value (in pixels)")
+    height = models.PositiveIntegerField(blank=True, null=True, help_text="Forces the height to a certain value (in pixels)")
+    
+    render_template = 'feincmstools/content/imagemixin.html'
+    
+    class Meta:
+        abstract = True
